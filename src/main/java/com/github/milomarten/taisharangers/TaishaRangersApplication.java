02296978e9b7d011bf69443f50a509acd02f6ba1
Milo Marten <milomartenstreams@gmail.com>
2024-01-28 22:42:07 -0600

    Initial commit

---
+++ src/main/java/com/github/milomarten/taisharangers/TaishaRangersApplication.java
@@ -0,0 +1,13 @@
+package com.github.milomarten.taisharangers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaishaRangersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaishaRangersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/milomarten/taisharangers/TaishaRangersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.milomarten.taisharangers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaishaRangersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
