5a34c0f8abd4a812989f53d51e7fc883a03d0f0f
Milo Marten <milomartenstreams@gmail.com>
2024-04-01 00:38:51 -0500

    Lazy-load PTU CSV data

--- src/main/java/com/github/milomarten/taisharangers/services/PtuDataReader.java
+++ src/main/java/com/github/milomarten/taisharangers/services/PtuDataReader.java
@@ -3,40 +3,35 @@ package com.github.milomarten.taisharangers.services;
 import com.github.milomarten.taisharangers.models.csv.PTUAbility;
 import com.github.milomarten.taisharangers.models.csv.PTUMove;
 import com.github.milomarten.taisharangers.models.csv.PTUPokemon;
-import com.opencsv.CSVReader;
-import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import jakarta.annotation.PostConstruct;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Slf4j
 public class PtuDataReader {
-    private List<PTUPokemon> pokemon = new ArrayList<>();
-    private List<PTUMove> moves = new ArrayList<>();
-    private List<PTUAbility> abilities = new ArrayList<>();
+    @Getter(lazy = true)
+    private final List<PTUPokemon> pokemon = readData("pokemon", PTUPokemon.class);
+    @Getter(lazy = true)
+    private final List<PTUMove> moves = readData("move", PTUMove.class);
+    @Getter(lazy = true)
+    private final List<PTUAbility> abilities = readData("ability", PTUAbility.class);
 
-    @PostConstruct
-    private void init() throws Exception {
-        this.pokemon = new CsvToBeanBuilder<PTUPokemon>(new FileReader("src/main/resources/csv/pokemon-data.csv"))
-                .withType(PTUPokemon.class)
-                .build()
-                .parse();
-
-        this.moves = new CsvToBeanBuilder<PTUMove>(new FileReader("src/main/resources/csv/move-data.csv"))
-                .withType(PTUMove.class)
-                .build()
-                .parse();
-
-        this.abilities = new CsvToBeanBuilder<PTUAbility>(new FileReader("src/main/resources/csv/abilities-data.csv"))
-                .withType(PTUAbility.class)
-                .build()
-                .parse();
+    private <T> List<T> readData(String type, Class<T> clazz) {
+        try {
+            return new CsvToBeanBuilder<T>(new FileReader("src/main/resources/csv/" + type + "-data.csv"))
+                    .withType(clazz)
+                    .build()
+                    .parse();
+        } catch (FileNotFoundException e) {
+            log.error("Error reading file", e);
+            return new ArrayList<>();
+        }
     }
 }
